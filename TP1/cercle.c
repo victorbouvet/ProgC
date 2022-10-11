cdfb0de3aae88cf85b015610b22be275be94df14
Victor <91113788+victorbouvet@users.noreply.github.com>
2022-10-11 17:38:34 +0200

    Update cercle.c

--- TP1/cercle.c
+++ TP1/cercle.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-#define M_PI
+#define M_PI //utilisaiton de la bibliothèque math.h pour avoir la valeur exacte de pi
 
 int main(){
     float rayon =  5;
-    float aire = M_PI*(rayon*rayon);
-    float perimetre = 2*M_PI*rayon;
+    float aire = M_PI*(rayon*rayon); //formulaire de l'aire d'un cercle
+    float perimetre = 2*M_PI*rayon; //formulaire du perimetre d'un cercle
     printf("Aire du cercle : %f \nPerimetre du cercle : %f\n",aire,perimetre);
 }
 
